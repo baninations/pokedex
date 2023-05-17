620b560479d50459161471a946ba5674a1561c18
baninations <baninations@gmail.com>
2023-05-17 11:03:13 +0200

    Initial commit

---
+++ js/scripts.js
@@ -0,0 +1,22 @@
+let myName = "Alban"
+
+document.write(myName)
+
+
+let simpleAddition = 2 + 2;
+
+document.write(simpleAddition)
+
+let a = 5;
+let b = 8;
+let result;
+
+result = a + b;
+
+document.write(result);
+
+let size = 100;
+let doubleSize = size * 2;
+let minSize = (doubleSize * 2) - (size/2);
+
+document.write(minSize)
