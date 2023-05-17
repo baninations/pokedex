ec37fab23873b95565470b11734ea364e5758a34
baninations <baninations@gmail.com>
2023-05-17 11:06:43 +0200

    Created the required scripts.js file

--- js/scripts.js
+++ js/scripts.js
@@ -1,22 +1,4 @@
-let myName = "Alban"
+alert("Hello world")
 
-document.write(myName)
-
-
-let simpleAddition = 2 + 2;
-
-document.write(simpleAddition)
-
-let a = 5;
-let b = 8;
-let result;
-
-result = a + b;
-
-document.write(result);
-
-let size = 100;
-let doubleSize = size * 2;
-let minSize = (doubleSize * 2) - (size/2);
-
-document.write(minSize)
+let favoriteFood = 'Proper Pizza';
+document.write(favoriteFood);
