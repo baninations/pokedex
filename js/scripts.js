53ff4ba5e25e567180de9a26211684edcee7c466
baninations <baninations@gmail.com>
2023-05-27 22:31:50 +0200

    Created pokemonList array and filled it with data

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,7 @@
-alert("Hello world")
-
-let favoriteFood = 'Proper Pizza';
-document.write(favoriteFood);
+let pokemonList = [
+    {name: 'Pikachu', height: 4, type: 'Electric'},
+    {name: 'Charmander', height: 6, type: 'Fire'},
+    {name: 'Charizard', height: 17, type: ['Fire','Flying']},
+    {name: 'Ninetales', height: 11, type: 'Fire'},
+    {name: 'Rapidash', height: 17, type: 'Fire'}
+]
