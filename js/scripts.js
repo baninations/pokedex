e4c0b8cf189fa1e4aae1195883975b8d5b010b8b
baninations <baninations@gmail.com>
2023-05-28 22:32:49 +0200

    fixed spacing issues and fixed the datatypes in the pokemonList

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,7 @@
 let pokemonList = [
-    {name: 'Pikachu', height: 4, types: 'Electric'},
-    {name: 'Charmander', height: 6, types: 'Fire'},
-    {name: 'Charizard', height: 17, types: ['Fire','Flying']},
-    {name: 'Ninetales', height: 11, types: 'Fire'},
-    {name: 'Rapidash', height: 17, types: 'Fire'}
+    {name: 'Pikachu', height: 4, types: ['Electric']},
+    {name: 'Charmander', height: 6, types: ['Fire']},
+    {name: 'Charizard', height: 17, types: ['Fire', 'Flying']},
+    {name: 'Ninetales', height: 11, types: ['Fire']},
+    {name: 'Rapidash', height: 17, types: ['Fire']}
 ];
