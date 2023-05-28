32dc1f872bb21bad30235051a7a402c6f21fa053
baninations <baninations@gmail.com>
2023-05-28 18:45:46 +0200

    Renamed type to types and added semicolon;

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,7 @@
 let pokemonList = [
-    {name: 'Pikachu', height: 4, type: 'Electric'},
-    {name: 'Charmander', height: 6, type: 'Fire'},
-    {name: 'Charizard', height: 17, type: ['Fire','Flying']},
-    {name: 'Ninetales', height: 11, type: 'Fire'},
-    {name: 'Rapidash', height: 17, type: 'Fire'}
-]
+    {name: 'Pikachu', height: 4, types: 'Electric'},
+    {name: 'Charmander', height: 6, types: 'Fire'},
+    {name: 'Charizard', height: 17, types: ['Fire','Flying']},
+    {name: 'Ninetales', height: 11, types: 'Fire'},
+    {name: 'Rapidash', height: 17, types: 'Fire'}
+];
