77e82d85471e3e56c83b3ab77feca0ad8360d9ac
baninations <baninations@gmail.com>
2023-05-30 21:28:58 +0200

    Displayed Pokémon

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,21 @@
 let pokemonList = [
-    {name: 'Pikachu', height: 4, types: ['Electric']},
-    {name: 'Charmander', height: 6, types: ['Fire']},
-    {name: 'Charizard', height: 17, types: ['Fire', 'Flying']},
-    {name: 'Ninetales', height: 11, types: ['Fire']},
-    {name: 'Rapidash', height: 17, types: ['Fire']}
+    {name: 'Pikachu', height: 0.4, types: ['Electric']},
+    {name: 'Charmander', height: 0.6, types: ['Fire']},
+    {name: 'Charizard', height: 1.7, types: ['Fire', 'Flying']},
+    {name: 'Ninetales', height: 1.1, types: ['Fire']},
+    {name: 'Rapidash', height: 1.7, types: ['Fire']}
 ];
+
+for(let i = 0;i<pokemonList.length;i++) {
+    if(pokemonList[i].height > 1) {
+        document.write(
+            pokemonList[i].name + " (height: " + pokemonList[i].height + ") Wow that's big<br>"
+        )
+    } else {
+        document.write(
+            pokemonList[i].name + " (height: " + pokemonList[i].height + ") <br>"
+        )
+    }
+}
+
+
