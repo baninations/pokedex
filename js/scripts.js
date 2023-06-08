150382a3e3b8230d80205512ffb217a688ebb6b7
baninations <baninations@gmail.com>
2023-06-08 12:26:55 +0200

    forEach loop applied and wrapped pokemonList with IIFE

--- js/scripts.js
+++ js/scripts.js
@@ -10,11 +10,11 @@ for(let i = 0;i<pokemonList.length;i++) {
     if(pokemonList[i].height > 1) {
         document.write(
             pokemonList[i].name + " (height: " + pokemonList[i].height + ") Wow that's big<br>"
-        )
+        );
     } else {
         document.write(
             pokemonList[i].name + " (height: " + pokemonList[i].height + ") <br>"
-        )
+        );
     }
 }
 
