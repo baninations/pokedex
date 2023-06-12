79cfda71e0988e6035e4885502cc3734a166ed6f
baninations <baninations@gmail.com>
2023-06-12 21:15:18 +0200

    refactored the code

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,21 @@
 let pokemonRepository = (function(){
     let pokemonList = [
-        {name: 'Pikachu', height: 0.4, types: ['Electric']},
-        {name: 'Charmander', height: 0.6, types: ['Fire']},
-        {name: 'Charizard', height: 1.7, types: ['Fire', 'Flying']},
-        {name: 'Ninetales', height: 1.1, types: ['Fire']},
-        {name: 'Rapidash', height: 1.7, types: ['Fire']}
+        {name: "Pikachu", height: 0.4, types: ["Electric"]},
+        {name: "Charmander", height: 0.6, types: ["Fire"]},
+        {name: "Charizard", height: 1.7, types: ["Fire", "Flying"]},
+        {name: "Ninetales", height: 1.1, types: ["Fire"]},
+        {name: "Rapidash", height: 1.7, types: ["Fire"]}
     ];
-    let ul = document.querySelector(".pokemon-list").append("Pokedex")
+
+    let pokemonListTitle = document.querySelector(".pokemon-list")
+    pokemonListTitle.append("Pokedex")
 
     function addListItem(pokemon) {
         let ul = document.querySelector(".pokemon-list")
         let li = document.createElement("li")
         let button = document.createElement("button")
-        let finalBtn = button.innerText = pokemon.name
-        let buttonClass = button.classList.add("btn")
+        button.innerText = pokemon.name
+        button.classList.add("btn")
         li.append(button)
         ul.append(li)
         button.addEventListener("click", e => {
