c1b94deebf84eca984cffcceba73563cc4cc4461
baninations <baninations@gmail.com>
2023-06-12 11:30:59 +0200

    DOM manipulation
    DOM manipulation to create button list and print the selected button to the console

--- js/scripts.js
+++ js/scripts.js
@@ -6,32 +6,39 @@ let pokemonRepository = (function(){
         {name: 'Ninetales', height: 1.1, types: ['Fire']},
         {name: 'Rapidash', height: 1.7, types: ['Fire']}
     ];
+    let ul = document.querySelector(".pokemon-list").append("Pokedex")
+
+    function addListItem(pokemon) {
+        let ul = document.querySelector(".pokemon-list")
+        let li = document.createElement("li")
+        let button = document.createElement("button")
+        let finalBtn = button.innerText = pokemon.name
+        let buttonClass = button.classList.add("btn")
+        li.append(button)
+        ul.append(li)
+        button.addEventListener("click", e => {
+            showDetails(pokemon)
+        })
+    }
+    
+    function showDetails(pokemon){
+        console.log(pokemon)
+    }
+    showDetails()
+
     return {
         add: function(pokemon) {
             pokemonList.push(pokemon);
         },
         getAll: function() {
             return pokemonList;
+        },
+        addListItem: function(pokemon) {
+            return addListItem(pokemon)
         }
     };
 })();
 
 pokemonRepository.getAll().forEach(function(pokemon){
-    if(pokemon.height > 1) {
-        displayBigPokemon(pokemon)
-    } else {
-        displayPokemon(pokemon)
-    }
+    pokemonRepository.addListItem(pokemon)
 })
-
-function displayBigPokemon(pokemon) {
-    document.write(
-        pokemon.name + " (height: " + pokemon.height + ") Wow that's big <br>"
-    );
-}
-
-function displayPokemon(pokemon) {
-    document.write(
-        pokemon.name + " (height: " + pokemon.height + ") <br>"
-    );
-}
